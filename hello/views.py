d2a9505f32524b938ea9162d17d3675b04ecd9ad
Krishna <krishna@Krishnas-MacBook-Pro.local>
2017-09-21 03:26:43 -0400

    test login

--- hello/views.py
+++ hello/views.py
@@ -13,8 +13,8 @@ def index(request):
 
 
 def live_map(request):
-    username = request.POST['username']
-    password = request.POST['password']
+    username = request.POST.get('username')
+    password = request.POST.get('password')
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
