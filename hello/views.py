b489cc0dc56573006f571c680fc7d7640ee8d352
Krishna <krishna@Krishnas-MacBook-Pro.local>
2017-09-21 03:08:06 -0400

    test login

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from django.contrib.auth.decorators import login_required
-
 from .models import Greeting
 
 # Create your views here.
@@ -11,7 +9,6 @@ def index(request):
     return render(request, 'index.html')
 
 
-@login_required
 def live_map(request):
     return render(request, 'map.html')
 
